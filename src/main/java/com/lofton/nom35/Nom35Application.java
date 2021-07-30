403232b02704f1f7465c71f136fa7025a415bd51
MKTSTF05 <MKTSTF05@Lap115.Lofton.com>
2021-07-30 11:24:52 -0500

    Cambio en las bases de datos de prueba

--- src/main/java/com/lofton/nom35/Nom35Application.java
+++ src/main/java/com/lofton/nom35/Nom35Application.java
@@ -2,26 +2,19 @@ package com.lofton.nom35;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Nom35Application {
-    // extends SpringBootServletInitializer
+public class Nom35Application extends SpringBootServletInitializer{
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Nom35Application.class);
-//    }
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(Nom35Application.class);
+   }
     public static void main(String[] args) {
         SpringApplication.run(Nom35Application.class, args);
-//        for(int x=1; x<=5; x++){
-//        for (int y=119;y<=132;y++ ){
-//            System.out.println("( 0,"+x+","+y+"),");
-//        }
-//        }
+
     }
 
 }
